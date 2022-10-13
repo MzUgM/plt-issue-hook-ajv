54604620d1473d7065757c312dc6033fa4f77495
MzUgM <108896003+MzUgM@users.noreply.github.com>
2022-10-13 14:05:15 -0700

    add 'foo=bar' to output

--- plugin.js
+++ plugin.js
@@ -2,9 +2,19 @@
 
 module.exports = async function (app) {
   app.platformatic.addEntityHooks('demo', {
+    // application owns input to the database
     save: async (save, opts) => {
       const patched = { ...opts.input, hookOverrides: 'always this string' }
       return save({ ...opts, input: patched })
+    },
+
+    // add additional field to output
+    find: async (find, opts) => {
+      const res = await find(opts)
+      return res.map((row) => {
+        row.foo = 'bar'
+        return row
+      }) 
     }
   })
 }
